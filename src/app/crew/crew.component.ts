374aef1bb5fc77c0a101e38f4b3f94906e99c4fb
jennyryoo <jennyjryoo@gmail.com>
2022-07-22 16:14:57 -0700

    finished studio 20 part 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,11 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember (candidate) {
+    if (this.crew.includes(candidate) === false && this.crew.length <3) {
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      this.crew.splice(this.crew.indexOf(candidate));
+    }
+  }
 }
